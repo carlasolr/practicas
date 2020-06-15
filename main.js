e0c5818606a08f6e3b8d24e2ff32301cae72a2e7
carlasolr <carlasol_rocca@live.com>
2020-06-15 17:42:21 -0300

    funcional

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 window.onload = function(){
 	const botonMas = document.querySelector('.mas')
 	const botonMenos = document.querySelector('.menos')
-	const sillas = document.querySelector('.sillas')
+	const sillas = document.querySelectorAll('.sillas')
+
 
 	let contador = 0
 
 	function agregar(){
-			sillas.style = 'display: inline'
-			contador = contador - 1		
+			sillas[contador].style = 'visibility: visible'
+			contador = contador + 1		
 	}
 
 
 	function restar(){
-			sillas.style = 'display: none'
-			contador = contador + 1
+			sillas[contador - 1].style = 'visibility: hidden'
+			contador = contador - 1
 	}
 
 	botonMas.onclick= function(){
 		agregar()
+		console.log(sillas)
 	}
 
 	botonMenos.onclick= function(){
 		restar()
 	}
+	
+	
 }
